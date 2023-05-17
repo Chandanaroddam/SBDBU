431bf6f2848ed6ff3a1c9e87de966e6ae34efbd6
Chandanaroddam <133826175+Chandanaroddam@users.noreply.github.com>
2023-05-16 21:20:23 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def create_clothing_brand_page(brand_name, clothing_items):
+    st.title(f"{brand_name} Clothing")
+    st.write("Welcome to our online store!")
+    st.write("Check out our latest collection:")
+
+    for item in clothing_items:
+        st.write(item)
+
+    st.write("Thank you for visiting our website. Happy shopping!")
+
+# Example usage:
+brand_name = "Fashion World"
+clothing_items = ["T-shirt", "Jeans", "Dress", "Jacket", "Shoes"]
+
+create_clothing_brand_page(brand_name, clothing_items)
